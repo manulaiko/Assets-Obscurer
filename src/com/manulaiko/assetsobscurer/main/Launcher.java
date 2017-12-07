6d03b7ddd6610f7bd3f20d6f9605a2d5f698defd
Manulaiko <manulaiko@gmail.com>
2017-12-07 12:36:03 +0100

    Create Launcher

---
+++ src/com/manulaiko/assetsobscurer/main/Launcher.java
@@ -0,0 +1,72 @@
+package com.manulaiko.assetsobscurer.main;
+
+import com.manulaiko.tabitha.Application;
+import com.manulaiko.tabitha.arguments.Argument;
+import com.manulaiko.tabitha.log.Console;
+import com.manulaiko.tabitha.log.ConsoleManager;
+
+import java.util.Arrays;
+
+/**
+ * Launcher class.
+ * ===============
+ *
+ * Executes the application.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public class Launcher extends Application {
+    ///////////////////////////////////
+    // Static methods and properties //
+    ///////////////////////////////////
+
+    /**
+     * Console logger.
+     */
+    public static final Console console = ConsoleManager.forClass(Launcher.class);
+
+    /**
+     * Main method.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        Launcher launcher = new Launcher("Manulaiko", "Assets Obscurer", "0.0.0", args);
+        launcher.start();
+    }
+
+    ///////////////////////////////////////
+    // Non static methods and properties //
+    ///////////////////////////////////////
+
+    /**
+     * Constructor.
+     *
+     * @param author      Author name.
+     * @param application Application name.
+     * @param version     Application version.
+     * @param arguments   Command line arguments.
+     */
+    public Launcher(String author, String application, String version, String[] arguments) {
+        super(author, application, version, arguments);
+    }
+
+    /**
+     * Called when the application has finished bootstrapping.
+     */
+    @Override
+    public void onStarted() {
+
+    }
+
+    /**
+     * Returns the argument instances.
+     *
+     * @return Argument instances.
+     */
+    @Override
+    protected Iterable<Argument> _arguments() {
+        return Arrays.asList(
+        );
+    }
+}
